1f632cd24008ec545b5f4b395ff55ff6dea35642
Neeraj Gurung <neerajgurung2099@gmail.com>
2023-07-13 19:02:26 +0545

    Update on Side Nav

--- src/components/DynamicIcon.tsx
+++ src/components/DynamicIcon.tsx
@@ -1,5 +1,7 @@
 import { CSSProperties, SVGAttributes, Suspense, lazy } from "react";
 import { IconContext } from "react-icons/lib";
+import { CiSquareQuestion } from "react-icons/ci";
+import * as CI from "react-icons/ci";
 
 type Props = {
   icon: string;
@@ -10,14 +12,22 @@ type Props = {
   style?: CSSProperties;
   attr?: SVGAttributes<SVGElement>;
 };
+
 const DynamicIcon: React.FC<Props> = (props) => {
   const library = props.library.toLowerCase();
-
   const path = `react-icons/${library}`;
-  const Icon = lazy(async () => {
-    const module = await import(path);
-    return { default: module[props.icon] };
-  });
+  // const Icon = lazy(async () => await import(/* webpackMode: "eager" */ path));
+
+  console.log(CI);
+  // const Icon = lazy(async () => {
+  //   try {
+  //     const module = await import(path);
+  //     return { default: module[props.icon] };
+  //   } catch (ex) {
+  //     console.log(ex);
+  //     return { default: CiSquareQuestion };
+  //   }
+  // });
 
   const value: IconContext = {
     color: props.color,
@@ -27,9 +37,9 @@ const DynamicIcon: React.FC<Props> = (props) => {
     attr: props.attr,
   };
   return (
-    <Suspense>
+    <Suspense fallback={null}>
       <IconContext.Provider value={value}>
-        <Icon />
+        <CiSquareQuestion />
       </IconContext.Provider>
     </Suspense>
   );
--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,20 +1,19 @@
 import { Link } from "react-router-dom";
 import DynamicIcon from "./DynamicIcon";
 type Props = {
-  icon: string;
-  library: string;
   path: string;
+  name: string;
+  children: any;
 };
 
 const NavItem: React.FC<Props> = (props) => {
   return (
-    <Link to={props.path} className="flex items-center ">
-      <DynamicIcon
-        icon={props.icon}
-        library={props.library}
-        className="inline-block mr-2"
-      />
-      <span className="font-normal">Home</span>
+    <Link
+      to={props.path}
+      className="flex items-center m-3 hover:bg-gray-200 p-2 rounded-lg "
+    >
+      {props.children}
+      <span className="text-lg">{props.name}</span>
     </Link>
   );
 };
